ba1be1afb08c01f0ca514af00d7c04968ce80d4e
Zin <zinedinemokhefi@gmail.com>
2025-06-07 14:19:50 +0200

    Added HomeWork

--- homework_processAPI/homework3.c
+++ homework_processAPI/homework3.c
@@ -7,7 +7,7 @@
 int rc_wait;
 
 //YAVIN4, REBELL-BASE!
-int main(int rc , char argv[]){
+int main(int argc , char argv[]){
     int fc = fork();
 
     if (fc < 0){
@@ -18,7 +18,7 @@ int main(int rc , char argv[]){
     if (fc == 0){
         printf("Child Process: %d | HELLO!!\n\r", getpid());
     } else {
-        rc_wait = waitpid(getpid());
+        rc_wait = waitpid(fc, NULL, NULL);
         printf("Parent of %d , PARENT:%d | GOODBYE!! \n\r", rc, getpid());
     }
     return(0);
