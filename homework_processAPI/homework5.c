6990ff212fd4f95e6687696e295684b14dc676b9
Zin <zinedinemokhefi@gmail.com>
2025-06-09 03:06:44 +0200

    HW

--- homework_processAPI/homework5.c
+++ homework_processAPI/homework5.c
@@ -1,1 +1,9 @@
-// PIPE , PAGE  55 8) Finish this later
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char argv[]){
+    // PIPE , PAGE  55 8) Finish this later, cause its fuckin 3 am    
+}
