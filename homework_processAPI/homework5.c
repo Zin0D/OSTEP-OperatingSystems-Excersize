eb5ecda8f70dac218f1986dc5cdd8aeb80aa5c77
Zin <zinedinemokhefi@gmail.com>
2025-06-11 17:10:20 +0200

    Finished all HomeWork of Code in Part 1 of 3 of the book

--- homework_processAPI/homework5.c
+++ homework_processAPI/homework5.c
@@ -7,10 +7,42 @@
 
 int status;
 char buffer[256] = {0};
-char msg[] = "Hello from Child1":
+char msg[] = "Hello from Child1";
 
-int main(int argc, char argv[]){
+int main(int argc, int argv[]) {
     printf("Initialize the Pipe\n\r");
     int pipefds[2];
-    pipe(pipefds)
+    pipe(pipefds);
+
+    int rc = fork();
+    if (rc < 0){
+        fprintf(stderr,"Failed to create child\n\r");
+        exit(1);
+    }
+
+    if (rc == 0){
+        printf("Child created, PID of piping child: %d \n\r", getpid());
+        write(pipefds[1], msg, 18);
+        int rd = fork();
+
+        if (rd < 0){
+            fprintf(stderr,"Failed to create child2\n\r");
+            exit(1);
+        }
+
+        if (rd == 0){
+            printf("Second Child, PID: %d \n\r", getpid());
+            read(pipefds[0], buffer, 18);
+            printf("BUFF Received: %s ,current PID: %d  \n\r", buffer, getpid());
+        }
+        else {
+            waitpid(rd, &status, 0);
+            printf("Finished Parent2\n\r");
+        }
+
+    } else {
+        waitpid(rc, &status, 0);
+        printf("Finished Parent\n\r");
+    }
+    return 0;
 }
