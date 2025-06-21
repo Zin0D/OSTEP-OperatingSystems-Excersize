ef4dce50949eb4da15187fafa7711263a643fabc
Zin <zinedinemokhefi@gmail.com>
2025-06-21 16:09:43 +0200

    VirtualAdressSpaces Incoming

--- VirtualAdressSpaces/va.c
+++ VirtualAdressSpaces/va.c
@@ -3,8 +3,8 @@
 
 int main(int argc, int argv[]){
     printf("location of code: 0x%p\n\r", main);
-    printf("location of heap 0x%p\n\r", malloc(100));
-    char x[] = "S4veTh3eW0rld";
-    printf("location of stack; 0x%p\n\r", &x );
+    printf("location of heap 0x%p\n\r", malloc(100)); //Heap allocated element, malloc seems to return an adress.
+    char x[] = "S4veTh3eW0rld"; //First allocated variable that is stored inside the stack, as the stack keeps variables of each function on it.
+    printf("location of stack; 0x%p\n\r", &x ); 
     return 0;
 }
