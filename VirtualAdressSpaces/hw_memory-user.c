959b15ac8fb7ed7a4d0f8d844a210afe0a7a294e
Zin <zinedinemokhefi@gmail.com>
2025-06-21 18:18:23 +0200

    HomeWork added

---
+++ VirtualAdressSpaces/hw_memory-user.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define BUFFSIZE 2048
+
+int main(int argc, char *argv[]){
+
+    char x[] = "S4veTh3eW0rld"; 
+    printf("location of code: 0x%p\n\r", main);
+    printf("location of heap 0x%p\n\r", malloc(100)); 
+    printf("location of stack; 0x%p\n\r", &x ); 
+
+    if (argc != 4){
+        printf("Usage ./homework-user.c <MEGABYTES> <Iterrations> <ValueAtStartOfArray>  ");
+        exit(1);
+    }
+    printf("Starting Execution\n\r");
+        
+    int arr[BUFFSIZE] = {0};
+    int track = 0;
+    int value = atoi((argv[3]));
+
+
+    while (track < BUFFSIZE)
+    {
+        arr[track] = value;
+        printf("Element in arr: %d\r", arr[track]);
+        sleep(0.1);
+        value = value + 3;
+        track++;
+    }
+
+    if (arr[0] != atoi(argv[3])){
+        printf("Your code is flawed: ");
+        exit(1);
+    }
+    
+    
+    int holder = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("\n\r");
+    printf("holder: %d\n\r", holder); 
+    int n = 0;
+
+    for (int i = 0; i < atoi(argv[2]); i++) {
+            int j = 0;
+            while(j < holder){
+                printf("Element at Index: %d IS: %d \r", j, arr[j]);
+                arr[j] = arr[j] + 1;
+                j++; 
+                sleep(0.1);
+
+                if (j == BUFFSIZE -1 ){
+                    n++;
+                    printf("Finished calculations: %d \r", n);
+                    sleep(0.5);
+                    printf("New value of nth element at index: %d is value: %d\r", BUFFSIZE-1, arr[j]);
+                    sleep(0.5);
+                }
+            }
+    }
+    printf("\n\r%s\n\r", x);
+    return 0;
+}
