50fcdad32205b9e7d7d6e81a2be0972355bfa3ed
Zin <zinedinemokhefi@gmail.com>
2025-06-21 15:58:55 +0200

    VirtualAdressSpaces Incoming

--- VirtualAdressSpaces/va.c
+++ VirtualAdressSpaces/va.c
@@ -5,6 +5,6 @@ int main(int argc, int argv[]){
     printf("location of code: 0x%p\n\r", main);
     printf("location of heap 0x%p\n\r", malloc(100));
     char x[] = "S4veTh3eW0rld";
-    printf("location of stack; ßx%p\n\r", &x );
+    printf("location of stack; 0x%p\n\r", &x );
     return 0;
 }
